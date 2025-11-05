bd2614a3e1e54d3d744867611d839869eca1cf3a
Bakheet Ahmed Al-Zahrani <bakheet@gmail.com>
2025-11-06 00:50:06 +0300

    fix(smart-parser.cjs): require CJS logger path

--- tools/smart-parser.cjs
+++ tools/smart-parser.cjs
@@ -2,7 +2,7 @@
 const path = require('path');
 const { spawnSync } = require('child_process');
 const FSHelpers = require('../core/utils/fs.cjs');
-const { Logger } = require('./lib/log');
+const { Logger } = require('./lib/log.cjs');
 const { HTMLParser } = require('../src/parsers/html-parser');
 const HTMLExtractor = require('../core/canonical/extract'); const ParserHelpers = require('../core/canonical-helpers');
 
@@ -90,6 +90,7 @@ if (require.main === module){
     process.exit(0);
   })();
 }
+
 
 
 
